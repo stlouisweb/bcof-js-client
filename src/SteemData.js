d74dd43f5d73d4021aca463824e1abf936aacfbc
stlouisweb <stlouisweb@gmail.com>
2017-09-05 09:52:14 -0500

    login and create transactions.

--- src/SteemData.js
+++ src/SteemData.js
@@ -17,227 +17,51 @@ class SteemData extends Component {
       "chain_id",
       "18dcf0a285365fc58b71f18b3d3fec954aa0c141c44e4e5cb4cf777b9eab274e"
     );
+    steem.config.set("address_prefix", "TST");
 
-    steem.api.getAccounts(["jeremy"], function(err, response) {
-      console.log(response);
-      let users = response.map(user => {
-        return { name: user.name, id: user.id, balance: user.balance };
-      });
-      self.setUsers(users);
-    });
+    const username = "chris";
+    const password = "bcof";
+    const privActiveWif = steem.auth.toWif(username, password, "active");
 
-    // var wif = steem.auth.toWif(username, password, "posting");
-    // steem.broadcast.vote(wif, voter, author, permlink, weight, function(
-    //   err,
-    //   result
-    // ) {
-    //   console.log(err, result);
-    // });
+    /** Add posting key auth */
+    steem.broadcast.addKeyAuth(
+      {
+        signingKey: privActiveWif,
+        username,
+        authorizedKey: "TST8EFtW1mscEsH2QWcYpqjFmETPUTDEWgbSRnRCrkJbNxj68bcmh",
+        role: "posting"
+      },
+      (err, result) => {
+        console.log(err, result);
+      }
+    );
+    console.log("get accounts");
 
-    steem.api.getApiByName("tracker_api", function(err, result) {
+    steem.api.getAccounts(["chris"], function(err, result) {
       console.log(err, result);
     });
 
-    const WIF = steem.auth.toWif("jeremy", "", "posting");
-    console.log(WIF);
-
-    steem.broadcast.profileCreate(WIF, "", "", "", "Farmer White", function(
-      err,
-      result
-    ) {
+    steem.api.login("chris", "bcof", function(err, result) {
+      console.log("logged in");
       console.log(err, result);
     });
 
-    // var json = JSON.stringify([
-    //   {
-    //     account_options: "",
-    //     crop_assertion: "",
-    //     id: "",
-    //     location: "",
-    //     name: "Farmer White"
-    //   }
-    // ]);
-    // steem.broadcast.customJson(
-    //   WIF,
-    //   [], // Required_auths
-    //   [], // Required Posting Auths
-    //   "create_profile", // Id
-    //   json, //
-    //   function(err, result) {
-    //     console.log(err, result);
-    //   }
-    // );
-
-    // ------------------------------------------------------------------------
-
-    // steem.broadcast.accountCreate(
-    //   WIF,
-    //   "0.000 TESTS",
-    //   4,
-    //   "TestAcc",
-    //   "",
-    //   true,
-    //   true,
-    //   "",
-    //   {},
-    //   function(err, result) {
-    //     console.log(err, result);
-    //   }
-    // );
-
-    // steem.api.createProfile(
-    //   {
-    //     required_auths: [],
-    //     required_posting_auths: [],
-    //     name: "Farmer White",
-    //     account_options: "",
-    //     location: "",
-    //     crop_assertion: ""
-    //   },
-    //   function(err, result) {
-    //     console.log(err, result);
-    //   }
-    // );
-
-    steem.api.getProfile(
-      {
-        name: "Farmer Brown"
-      },
+    steem.broadcast.customJson(
+      privActiveWif,
+      [],
+      ["chris"],
+      "tracker",
+      '["create_profile", { "name": "Farmer Jones", "account_options": "", "location": "", "crop_assertion": ""}]',
       function(err, result) {
+        console.log("got profile");
         console.log(err, result);
       }
     );
-    steem.api.getProfile(
-      {
-        name: "Farmer White"
-      },
-      function(err, result) {
-        console.log(err, result);
-      }
-    );
-    steem.api.getProfile(
-      {
-        name: "Farmer Green"
-      },
-      function(err, result) {
-        console.log(err, result);
-      }
-    );
-  }
-
-  setUsers(users) {
-    this.setState({ users: users });
   }
 
   render() {
-    let userList = this.state.users.map(user => {
-      return (
-        <tr key={user.id}>
-          <td>
-            {user.id}
-          </td>
-          <td>
-            {user.name}
-          </td>
-          <td>
-            {user.balance}
-          </td>
-        </tr>
-      );
-    });
-    return (
-      <div>
-        {this.state.users.length > 0
-          ? <table>
-              <thead>
-                <tr>
-                  <th>User ID</th>
-                  <th>User Name</th>
-                  <th>Balance</th>
-                </tr>
-              </thead>
-              <tbody>
-                {userList}
-              </tbody>
-            </table>
-          : <p>Loading users...</p>}
-      </div>
-    );
+    return <div />;
   }
 }
 
 export default SteemData;
-
-//
-// steem.broadcast.send(
-//   {
-//     extensions: [],
-//     operations: [
-//       [
-//         "vote",
-//         {
-//           voter: "sisilafamille",
-//           author: "siol",
-//           permlink: "test",
-//           weight: 1000
-//         }
-//       ]
-//     ]
-//   },
-//   [privWif1, privWif2],
-//   (err, result) => {
-//     console.log(err, result);
-//   }
-// );
-
-//   console.log("getting profile");
-//   steem.api.getProfile(
-//     {
-//       required_auths: [],
-//       required_posting_auths: [],
-//       name: "Farmer Brown",
-//       account_options: "",
-//       location: "",
-//       crop_assertion: ""
-//     },
-//     function(err, result) {
-//       console.log("got profile");
-//       console.log(err, result);
-//     }
-//   );
-//   steem.api.createProfile(
-//     {
-//       required_auths: [],
-//       required_posting_auths: [],
-//       name: "Farmer White",
-//       account_options: "",
-//       location: "",
-//       crop_assertion: ""
-//     },
-//     function(err, result) {
-//       console.log(err, result);
-//     }
-//   );
-// }
-
-// const privWif1 = "5K2LA2ucS8b1GuFvVgZK6itKNE6fFMbDMX4GDtNHiczJESLGRd8";
-// const privWif2 = "5JRaypasxMx1L97ZUX7YuC5Psb5EAbF821kkAGtBj7xCJFQcbLg";
-//
-// steem.broadcast.customJson(
-//   privWif1,
-//   [],
-//   ["jeremy"],
-//   "tracker",
-//   {
-//     create_evaluation: {
-//       evaluated_by: "Trucker Joe",
-//       assertion: 0,
-//       verdict: true,
-//       evidence: [0],
-//       data: {}
-//     }
-//   },
-//   function(err, result) {
-//     console.log(err, result);
-//   }
-// );
